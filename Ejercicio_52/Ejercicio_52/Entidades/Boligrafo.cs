d283e8b4c050df5e094fa65bd401279f74e285e8
413831 <alepleins3pp@gmail.com>
2019-05-25 17:20:49 -0300

    Ejercicio 52

---
+++ Ejercicio_52/Ejercicio_52/Entidades/Boligrafo.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Boligrafo : IAcciones
+    {
+        private ConsoleColor colorTinta;
+        private float tinta;
+
+        #region Propiedades
+
+        public ConsoleColor Color
+        {
+            get
+            {
+                return this.colorTinta;
+            }
+            set
+            {
+                this.colorTinta = value;
+            }
+        }
+
+        public float UnidadesDeEscritura
+        {
+            get
+            {
+
+
+            }
+            set
+            {
+
+            }
+        }
+
+        #endregion
+
+        #region Métodos
+
+        public Boligrafo(int unidades, ConsoleColor color)
+        {
+
+        }
+
+        public EscrituraWrapper Escribir(string texto)
+        {
+        }
+
+        public bool Recargar()
+        {
+            return true;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+        #endregion
+
+    }
+}
---
+++ Ejercicio_52/Ejercicio_52/Entidades/Lapiz.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Lapiz
+    {
+        public float tamanioMina;
+
+        #region Propiedades
+
+        public ConsoleColor Color
+        {
+            get
+            {
+
+            }
+            set
+            {
+
+            }
+        }
+
+        public float UnidadesDeEscritura
+        {
+            get
+            {
+
+            }
+            set
+            {
+
+            }
+        }
+
+        #endregion
+
+        #region Métodos
+
+        public EscrituraWrapper Escribir(string texto)
+        {
+
+        }
+
+        public Lapiz(int unidades)
+        {
+
+        }
+
+        public bool Recargar(int unidades)
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+        #endregion
+
+    }
+}
+C
