2ff76a49a6f85f2e99340565437daa83d2e39ab2
413831 <alepleins3pp@gmail.com>
2019-06-17 14:45:06 -0300

    Ejercicio 64

---
+++ Ejercicio_64/Ejercicio_64/Ejercicio_64/Negocio.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Ejercicio_64
+{
+    public class Negocio
+    {
+        private Caja cajaUno;
+        private Caja cajaDos;
+        private List<string> clientes;
+
+        public Caja CajaUno
+        {
+            get
+            {
+                return this.cajaUno;
+            }
+        }
+
+        public Caja CajaDos
+        {
+            get
+            {
+                return this.cajaDos;
+            }
+        }
+
+        public List<string> Clientes
+        {
+            get
+            {
+                return this.clientes;
+            }
+        }
+
+        public void AsignarCaja()
+        {
+            Console.Write("Asignando cajas...");
+
+            foreach(string cliente in this.clientes)
+            {
+                if(this.cajaUno.FilaClientes.Count < this.cajaDos.FilaClientes.Count)
+                {
+                    this.cajaUno.FilaClientes.Add(cliente);
+                }
+                else
+                {
+                    this.cajaDos.FilaClientes.Add(cliente);
+                }
+                Thread.Sleep(1000);
+            }
+        }
+
+        public Negocio(Caja cajaUno, Caja cajaDos)
+        {
+            this.cajaUno = cajaUno;
+            this.cajaDos = cajaDos;
+            this.clientes = new List<string>();
+        }
+    }
+}
---
+++ Ejercicio_64/Ejercicio_64/Ejercicio_64/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Ejercicio_64
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Los threads (hilos) son métodos que reciben punteros a funciones para establecer su prioridad dentro del hilo principal
+            * 
+            *
+             */
+            Caja cajaUno = new Caja();
+            Caja cajaDos = new Caja();
+            Negocio negocio = new Negocio(cajaUno, cajaDos);
+            Thread asignacionCajas = new Thread(negocio.AsignarCaja);
+            Thread atenderCajaUno = new Thread(cajaUno.AtenderClientes);
+            Thread atenderCajaDos = new Thread(cajaDos.AtenderClientes);
+
+            negocio.Clientes.Add("Juancito");
+            negocio.Clientes.Add("Pepito");
+            negocio.Clientes.Add("Pedrito");
+            negocio.Clientes.Add("Manuelita");
+            negocio.Clientes.Add("Francisquito");
+
+            atenderCajaUno.Name = "Caja N°1";
+            atenderCajaDos.Name = "Caja N°2";
+
+            asignacionCajas.Start();
+            asignacionCajas.Join();
+            atenderCajaUno.Start();
+            atenderCajaDos.Start();
+
+            Console.ReadKey();
+        }
+    }
+}
