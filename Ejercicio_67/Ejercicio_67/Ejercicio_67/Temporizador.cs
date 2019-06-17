457b6f4294676872c251173f9213c65387e66acf
413831 <alepleins3pp@gmail.com>
2019-06-17 19:27:49 -0300

    Practica hilos

---
+++ Ejercicio_67/Ejercicio_67/Ejercicio_67/Temporizador.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Ejercicio_67
+{
+    public class Temporizador
+    {
+        private Thread hilo;
+        private int intervalo;
+
+        public bool Activo
+        {
+            get
+            {
+
+            }
+            set
+            {
+
+            }
+        }
+
+        public int Invervalo
+        {
+            get
+            {
+                return this.intervalo;
+            }
+            set
+            {
+                this.intervalo = value;
+            }
+        }
+
+        public void Corriendo()
+        {
+
+        }
+
+        public delegate void encargadoTiempo
+
+        public event EventoTiempo()
+    }
+}
---
+++ Practica_Hilos/Hilos/Hilos/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Hilos
+{
+    public partial class Form1 : Form
+    {
+        Thread hilo;
+        delegate void delegado(int valor);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIniciar_Click(object sender, EventArgs e)
+        {
+            hilo = new Thread(ProcesoUno);
+            hilo.Start();
+            hilo = new Thread(ProcesoDos);
+            hilo.Start();
+            hilo = new Thread(ProcesoTres);
+            hilo.Start();
+        }
+
+        public void ProcesoUno()
+        {
+            for (int i = 0; i < 101; i++)
+            {
+                delegado metodoDelegado = new delegado(ActualizarUno);
+                this.Invoke(metodoDelegado, new object[] { i });
+                Thread.Sleep(70);
+            }
+        }
+
+        public void ActualizarUno(int valor) // Modelo del delegado
+        {
+            barraProgresoUno.Value = valor;
+        }
+
+        public void ProcesoDos()
+        {
+            for (int i = 0; i < 101; i++)
+            {
+                delegado metodoDelegado = new delegado(ActualizarDos);
+                this.Invoke(metodoDelegado, new object[] { i });
+                Thread.Sleep(30);
+            }
+        }
+
+        public void ActualizarDos(int valor) // Modelo del delegado
+        {
+            barraProgresoDos.Value = valor;
+        }
+
+        public void ProcesoTres()
+        {
+            for (int i = 0; i < 101; i++)
+            {
+                delegado metodoDelegado = new delegado(ActualizarTres);
+                this.Invoke(metodoDelegado, new object[] { i });
+                Thread.Sleep(40);
+            }
+        }
+
+        public void ActualizarTres(int valor) // Modelo del delegado
+        {
+            barraProgresoTres.Value = valor;
+        }
+    }
+}
