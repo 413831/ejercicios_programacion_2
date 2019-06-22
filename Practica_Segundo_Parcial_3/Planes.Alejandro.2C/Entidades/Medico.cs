8d7adef8b84e143a0cde48566e1c138adbbfe1ea
413831 <alepleins3pp@gmail.com>
2019-06-22 18:47:14 -0300

    Practica Segundo Parcial 2

---
+++ Practica_Segundo_Parcial_3/Planes.Alejandro.2C/Entidades/Medico.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Entidades
+{
+    public delegate void FinAtencionPaciente(Paciente paciente, Medico medico);
+
+    public abstract class Medico : Persona
+    {
+       
+        public event FinAtencionPaciente AtencionFinalizada;
+
+        private Paciente pacienteActual;
+        protected static Random tiempoAleatorio;
+        protected Thread hilo;
+        protected Random numeroAleatorio;
+
+        #region Propiedades
+
+        public Paciente AtenderA
+        {
+            set
+            {
+                this.pacienteActual = value;
+            }
+        }
+
+        public virtual string EstaAtendiendoA
+        {
+            get
+            {
+                return this.pacienteActual.ToString();
+            }
+        }
+
+        #endregion
+
+        #region Método
+
+        protected abstract void Atender();
+
+        protected void FinalizarAtencion()
+        {
+            this.AtencionFinalizada(this.pacienteActual,this);
+            this.pacienteActual = null;
+        }
+
+        static Medico()
+        {
+            tiempoAleatorio = new Random();            
+        }
+
+        public Medico(string nombre, string apellido) : base(nombre,apellido)
+        {
+
+        }
+
+     
+
+        #endregion
+
+
+    }
+}
---
+++ Practica_Segundo_Parcial_3/Planes.Alejandro.2C/frmFinal/frmFinal.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Entidades;
+using System.Threading;
+
+namespace frmFinal
+{
+    public partial class frmFinal : Form
+    {
+        private delegate void AtencionPaciente(object sender, EventArgs e);
+
+        private MEspecialista medicoEspecialista;
+        private MGeneral medicoGeneral;
+        private Thread mocker;
+        private Queue<Paciente> pacienteEnEspera;
+        private Paciente paciente;
+
+        public frmFinal()
+        {
+            InitializeComponent();
+            this.medicoGeneral = new MGeneral("Luis", "Salinas");
+            this.medicoEspecialista = new MEspecialista("Jorge", "Iglesias", MEspecialista.Especialidad.Traumatologo);
+            this.pacienteEnEspera = new Queue<Paciente>();
+        }
+
+        private void frmFinal_Load(object sender, EventArgs e)
+        {
+            this.mocker = new Thread(this.MockPacientes);
+            this.mocker.Start();
+        }
+
+        private void frmFinal_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (this.mocker.IsAlive)
+            {
+                this.mocker.Abort();
+            }
+        }
+
+        private void MockPacientes()
+        {
+            this.pacienteEnEspera.Enqueue(new Paciente("Pepito", "Gonzalez", 1));
+            Thread.Sleep(5000);
+            this.pacienteEnEspera.Enqueue(new Paciente("Juan Carlos", "Pérez", 2));
+            Thread.Sleep(5000);
+            this.pacienteEnEspera.Enqueue(new Paciente("Manuelita", "Pepas"));
+            Thread.Sleep(5000);
+            this.pacienteEnEspera.Enqueue(new Paciente("Juancito", "Lopez", 3));
+            Thread.Sleep(5000);
+        }
+
+        private void AtenderPacientes(IMedico iMedico)
+        {
+            paciente = this.pacienteEnEspera.Dequeue();
+
+            iMedico.IniciarAtencion(paciente);
+            this.FinAtencion(paciente, (Medico)iMedico);
+        }
+
+        private void FinAtencion(Paciente paciente, Medico medico)
+        {
+            string mensaje = String.Format("Finalizó la atención de {0} por {1}!", paciente.ToString(), medico.ToString());
+
+            if (this.InvokeRequired)
+            {
+                FinAtencionPaciente delegado = new FinAtencionPaciente(this.FinAtencion);
+                object[] objects = new object[] { paciente, medico };
+                this.BeginInvoke(delegado, objects);
+                MessageBox.Show(mensaje);
+            }
+        }
+
+        private void btnMedicoGral_Click(object sender, EventArgs e)
+        {
+            this.medicoGeneral.AtencionFinalizada += this.FinAtencion;
+
+            if (this.pacienteEnEspera.Count > 0)
+            {
+                this.AtenderPacientes(this.medicoGeneral);
+            }
+        }
+
+        private void btnMedicoEsp_Click(object sender, EventArgs e)
+        {
+            this.medicoEspecialista.AtencionFinalizada += this.FinAtencion;
+
+            if (this.pacienteEnEspera.Count > 0)
+            {
+                this.AtenderPacientes(this.medicoEspecialista);
+            }
+        }
+    }
+}
